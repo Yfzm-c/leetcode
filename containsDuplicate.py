0531a99a6a26ad76124b0d318bc3e992a7494be2
xiaoyaxiaohanxin <50623110+xiaoyaxiaohanxin@users.noreply.github.com>
2020-06-07 00:15:58 +0800

    做后面的题时想到的新的解法
    只要将原先的列表set一下去重，在和原先的列表比较长度即可，如果长度不一样，那么意味着有重复的数字，直接返回Ture即可

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,6 +1,8 @@
 #给定一个整数数组，判断是否存在重复元素。如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 
 #示例: 输入：[1,2,3,1]   输出: true
 #难度：简单
+
+#解法一：
 from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
@@ -8,6 +10,20 @@ class Solution:
         for i in range(len(nums)-1):
             if(nums[i]==nums[i+1]):
                 return True
+        return False
+solution=Solution()
+a=input("请输入一些整数，其中以逗号分隔：")
+a=a.split(",")
+nums=[int(a[i]) for i in range(len(a))]
+print(solution.containsDuplicate(nums))
+
+
+#解法二：
+from typing import List
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if(len(nums)!=len(set(nums))):
+            return True
         return False
 solution=Solution()
 a=input("请输入一些整数，其中以逗号分隔：")
